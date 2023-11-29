32c9b19eae730691e28f6d16228d8bae1bc3e4bc
Donell <donell.gill@outlook.com>
2023-11-29 16:18:44 -0500

    Created User and Customer models

---
+++ db/migrate/20231129211803_create_customers.rb
@@ -0,0 +1,11 @@
+class CreateCustomers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :customers do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :name
+      t.string :address
+
+      t.timestamps
+    end
+  end
+end
